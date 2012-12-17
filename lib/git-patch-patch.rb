7f0139cfb3ad094586a25cac0dbbf5d1a1a64319
lacravate <lacravate@lacravate.fr>
2012-12-17 02:09:19 +0100

    PatchPatcher and PatchPatch; workflow controller; patch model

---
+++ lib/git-patch-patch.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+
+$:.unshift File.join(File.dirname(__FILE__), *%w[.. lib])
+
+require 'git-patch-patch/patch_patcher'
---
+++ lib/git-patch-patch/version.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+module Git
+  class Trifle
+    class PatchPatcher
+      VERSION = "0.0.1"
+    end
+  end
+end
